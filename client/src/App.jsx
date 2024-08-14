d4b43b87748619bd9a31d2101c189e7748e4a604
parit9222 <140873607+parit9222@users.noreply.github.com>
2024-08-14 10:34:50 +0530

    complete ui changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { tokenContract } from './contract/listen-contract.js';
 import { ethers } from 'ethers';
+import Navbar from './components/Navbar.jsx';
 
 const web3 = new Web3(Web3.givenProvider);
 
 export default function App() {
   const [walletAddress, setWalletAddress] = useState('');
+  const [walletAddressForBalance, setWalletAddressForBalance] = useState('');
   const [walletBalance, setWalletBalance] = useState('');
   const [isConnected, setIsConnected] = useState(false);
   const [file, setFile] = useState(null);
@@ -17,30 +19,34 @@ export default function App() {
   const [showDuplicatePopup, setShowDuplicatePopup] = useState(false); // State for duplicate confirmation
   const [rowCount, setRowCount] = useState(0);
   const [rowAmount, setRowAmount] = useState(0);
+  const [showDisconnectPopup, setShowDisconnectPopup] = useState(false);
+
   const fileRef = useRef(null);
+  const addressSize = 5;
 
   const [groupedTransferData, setGroupedTransferData] = useState([]);
   // console.log(groupedTransferData);
 
 
-  
+
 
   const handleDecimalsForArray = async (deci) => {
     const contractResult = await tokenContract();
 
-    if (walletAddress) {
-        const decimals = await contractResult.decimals();
-        const amountToSend = (BigInt(Math.floor(Number(deci) * Math.pow(10, decimals)))).toString();
-        return Number(amountToSend);
+    if (walletAddressForBalance) {
+      const decimals = await contractResult.decimals();
+      const amountToSend = (BigInt(Math.floor(Number(deci) * Math.pow(10, decimals)))).toString();
+      return Number(amountToSend);
     } else {
-        toast.error('array');
-        return null; // Return a default value or handle the error case as needed
+      toast.error('array');
+      return null;
     }
-};
+  };
+
 
   const handleTransferTokenFromExcel = async () => {
     const contractResult = await tokenContract();
-    if (walletAddress && groupedTransferData.length) {
+    if (walletAddressForBalance && groupedTransferData.length) {
       try {
         const decimals = await contractResult.decimals();
 
@@ -69,18 +75,32 @@ export default function App() {
 
   const getBalance = async () => {
     const contractResult = await tokenContract();
-    if (walletAddress && contractResult) {
-      const balanceOf = await contractResult.balanceOf(walletAddress);
+    if (walletAddressForBalance && contractResult) {
+      const balanceOf = await contractResult.balanceOf(walletAddressForBalance);
       const decimals = await contractResult.decimals();
       setWalletBalance(Number(balanceOf) / Math.pow(10, decimals));
     }
   };
 
   useEffect(() => {
-    if (walletAddress) {
+    if (walletAddressForBalance) {
       getBalance();
     }
-  }, [walletAddress]);
+  }, [walletAddressForBalance]);
+
+
+  const handleFormatWalletAddress = (address, charCount) => {
+
+    if (charCount === 0) {
+      return address;
+    } else {
+      const count = Math.max(1, charCount);
+      const start = address.slice(0, count);
+      const end = address.slice(-count);
+
+      return `${start}...${end}`;
+    }
+  }
 
   useEffect(() => {
     const checkWalletConnection = async () => {
@@ -88,7 +108,10 @@ export default function App() {
         try {
           const accounts = await window.ethereum.request({ method: 'eth_accounts' });
           if (accounts.length > 0) {
-            setWalletAddress(accounts[0]);
+
+            setWalletAddress(handleFormatWalletAddress(accounts[0], addressSize));
+            setWalletAddressForBalance(accounts[0]);
+
             setIsConnected(true);
           }
         } catch (error) {
@@ -149,7 +172,9 @@ export default function App() {
         if (switched) {
           const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
           const address = accounts[0];
-          setWalletAddress(address);
+
+          setWalletAddress(handleFormatWalletAddress(address, addressSize));
+          setWalletAddressForBalance(address);
           setIsConnected(true);
         }
       } catch (error) {
@@ -160,88 +185,102 @@ export default function App() {
     }
   };
 
-  const handleDisconnectMetamask = async () => {
+  const handleDisconnectMetamask = () => {
+    setShowDisconnectPopup(true);
+  };
+
+  const handleConfirmDisconnect = async () => {
     try {
       await window.ethereum.request({ method: 'wallet_revokePermissions', params: [{ eth_accounts: {} }] });
       setWalletAddress('');
       setWalletBalance('');
       setIsConnected(false);
+      setShowDisconnectPopup(false);
+      toast.success('Wallet disconnected successfully');
     } catch (error) {
       console.error("Error disconnecting from Metamask", error);
       toast.error('Error disconnecting from Metamask');
+      setShowDisconnectPopup(false);
     }
   };
+
+  const handleCancelDisconnect = () => {
+    setShowDisconnectPopup(false);
+    toast.info('Disconnect wallet canceled');
+  };
+
+
 
   const handleUpload = async () => {
     const groupSize = 2;
 
     if (file) {
-        const reader = new FileReader();
-        reader.onload = async (e) => {
-            const data = new Uint8Array(e.target.result);
-            const workbook = XLSX.read(data, { type: 'array' });
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
-            const jsonData = XLSX.utils.sheet_to_json(worksheet);
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-            const tokens = jsonData.map(row => row.tokens);
-            const amounts = jsonData.map(row => row.amount);
+        const tokens = jsonData.map(row => row.tokens);
+        const amounts = jsonData.map(row => row.amount);
 
-            const hasEmptyValues = tokens.some(token => !token) || amounts.some(amount => !amount);
+        const hasEmptyValues = tokens.some(token => !token) || amounts.some(amount => !amount);
 
-            if (hasEmptyValues) {
-                toast.error("The Excel sheet contains empty values. Please fill all values before uploading.");
-                return;
+        if (hasEmptyValues) {
+          toast.error("The Excel sheet contains empty values. Please fill all values before uploading.");
+          return;
+        }
+
+        if (tokens.length === amounts.length) {
+          const groupedData = [];
+          const tokenSet = new Set();
+          let hasDuplicates = false;
+          let hasInvalidAddresses = false;
+
+          for (let i = 0; i < tokens.length; i += groupSize) {
+            const group = [];
+            for (let j = i; j < i + groupSize && j < tokens.length; j++) {
+              const tokenAddress = tokens[j];
+              if (!ethers.utils.isAddress(tokenAddress)) {
+                hasInvalidAddresses = true;
+              }
+
+              const amount = await handleDecimalsForArray(amounts[j]);
+              group.push({ token: tokenAddress, amount });
+              // console.log(amounts[j]);
+
+              if (tokenSet.has(tokenAddress)) {
+                hasDuplicates = true;
+              } else {
+                tokenSet.add(tokenAddress);
+              }
             }
+            groupedData.push(group);
+          }
 
-            if (tokens.length === amounts.length) {
-                const groupedData = [];
-                const tokenSet = new Set();
-                let hasDuplicates = false;
-                let hasInvalidAddresses = false;
+          setGroupedTransferData(groupedData);
+          setRowCount(tokens.length);
+          const totalAmount = amounts.reduce((sum, value) => sum + value, 0);
+          const fixedDigitValue = 6;
+          setRowAmount(totalAmount.toFixed(fixedDigitValue));
+          // setRowAmount(totalAmount);
 
-                for (let i = 0; i < tokens.length; i += groupSize) {
-                    const group = [];
-                    for (let j = i; j < i + groupSize && j < tokens.length; j++) {
-                        const tokenAddress = tokens[j];
-                        if (!ethers.utils.isAddress(tokenAddress)) {
-                            hasInvalidAddresses = true;
-                        }
-
-                        const amount = await handleDecimalsForArray(amounts[j]);
-                        group.push({ token: tokenAddress, amount });
-                        // console.log(amounts[j]);
-
-                        if (tokenSet.has(tokenAddress)) {
-                            hasDuplicates = true;
-                        } else {
-                            tokenSet.add(tokenAddress);
-                        }
-                    }
-                    groupedData.push(group);
-                }
-
-                setGroupedTransferData(groupedData);
-                setRowCount(tokens.length);
-                const totalAmount = amounts.reduce((sum, value) => sum + value, 0);
-                const fixedDigitValue = 6;
-                setRowAmount(totalAmount.toFixed(fixedDigitValue));
-                // setRowAmount(totalAmount);
-
-                if (hasInvalidAddresses) {
-                    toast.error("The Excel sheet contains invalid addresses. Please correct them before uploading.");
-                } else if (hasDuplicates) {
-                    setShowDuplicatePopup(true);
-                } else {
-                    setShowPopup(true);
-                }
-            } else {
-                toast.error("Number of rows in 'tokens' and 'amount' columns do not match.");
-            }
-        };
-        reader.readAsArrayBuffer(file);
+          if (hasInvalidAddresses) {
+            toast.error("The Excel sheet contains invalid addresses. Please correct them before uploading.");
+          } else if (hasDuplicates) {
+            setShowDuplicatePopup(true);
+          } else {
+            setShowPopup(true);
+          }
+        } else {
+          toast.error("Number of rows in 'tokens' and 'amount' columns do not match.");
+        }
+      };
+      reader.readAsArrayBuffer(file);
     }
-};
+  };
 
   const handleConfirmUpload = () => {
     const reader = new FileReader();
@@ -278,7 +317,7 @@ export default function App() {
     // handleTransferTokenFromExcel();
 
     console.log(groupedTransferData);
-    
+
     setShowPopup(false);
   };
 
@@ -298,99 +337,114 @@ export default function App() {
   };
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <ToastContainer />
-      <h1 className='text-center font-bold mt-10'>Connect only BNB Testnet network wallet</h1>
-      <div className='flex flex-col gap-4 flex-1 mt-5'>
-        {isConnected ? (
-          <button
-            className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95'
-            onClick={handleDisconnectMetamask}
-          >
-            Disconnect Metamask
-          </button>
-        ) : (
-          <button
-            className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95'
-            onClick={handleConnectMetamask}
-          >
-            Connect Metamask
-          </button>
+    <div className="min-h-screen bg-gray-100">
+
+      <Navbar
+        isConnected={isConnected}
+        walletAddress={walletAddress}
+        walletBalance={walletBalance}
+        handleConnectMetamask={handleConnectMetamask}
+        handleDisconnectMetamask={handleDisconnectMetamask}
+      />
+
+      <div className="container max-w-lg mx-auto py-8">
+        {isConnected && (
+          <div className="bg-white p-8 rounded shadow-md text-center">
+            {/* <p className='text-xl font-semibold mb-6'>{walletBalance ? `Balance :- ${walletBalance} USDT` : `Your Wallet is not connected`}</p> */}
+              <h1 className="text-2xl font-semibold mb-6">Excel File Upload</h1>
+              <div className='border pt-3 rounded-lg'>
+                <input
+                  type="file"
+                  accept=".xlsx, .xls"
+                  ref={fileRef}
+                  onChange={(e) => setFile(e.target.files[0])}
+                  className="mb-4"
+                />
+                <button
+                  onClick={handleUpload}
+                  className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600"
+                >
+                  Upload
+                </button>
+              </div>
+          </div>
         )}
-      </div>
-      <div className='flex flex-col gap-4 flex-1 mt-5'>
-        <p>Wallet Address: {walletAddress}</p>
-      </div>
-      <div className='flex flex-col gap-4 flex-1 mt-5'>
-        <p>Wallet Balance: {walletBalance} USDT</p>
-      </div>
 
-      {isConnected && (
-        <div className="flex gap-4 mt-5">
-          <input
-            className='p-3 border border-gray-300 rounded w-full focus:outline-none'
-            type="file"
-            id='excel'
-            accept=".xlsx,.xls,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
-            ref={fileRef}
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <button
-            className='p-3 bg-blue-500 text-white rounded-lg uppercase hover:opacity-95'
-            onClick={handleUpload}
-          >
-            Upload File
-          </button>
-        </div>
-      )}
+        <ToastContainer />
 
-      {showPopup && (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60'>
-          <div className='bg-white p-6 rounded-lg shadow-lg max-w-sm w-full'>
-            <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Upload Confirmation</h2>
-            <p className='text-gray-700 mb-2'><span className='font-medium'>Number of Tokens :</span> {rowCount}</p>
-            <p className='text-gray-700 mb-4'><span className='font-medium'>Total Sum of Amount :</span> {rowAmount}</p>
-            <div className='flex justify-end gap-3'>
-              <button
-                className='px-4 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700 transition'
-                onClick={handleConfirmUpload}
-              >
-                Confirm
-              </button>
-              <button
-                className='px-4 py-2 bg-red-600 text-white rounded-lg text-sm font-medium hover:bg-red-700 transition'
-                onClick={handleCancelUpload}
-              >
-                Cancel
-              </button>
+        {showPopup && (
+          <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60'>
+            <div className='bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+              <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Upload Confirmation</h2>
+              <p className='text-gray-700 mb-2'><span className='font-medium'>Number of Tokens :</span> {rowCount}</p>
+              <p className='text-gray-700 mb-4'><span className='font-medium'>Total Sum of Amount :</span> {rowAmount}</p>
+              <div className='flex justify-end gap-3'>
+                <button
+                  className='px-4 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700 transition'
+                  onClick={handleConfirmUpload}
+                >
+                  Confirm
+                </button>
+                <button
+                  className='px-4 py-2 bg-red-600 text-white rounded-lg text-sm font-medium hover:bg-red-700 transition'
+                  onClick={handleCancelUpload}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {showDuplicatePopup && (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60'>
-          <div className='bg-white p-6 rounded-lg shadow-lg max-w-sm w-full'>
-            <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Duplicate Tokens Detected</h2>
-            <p className='text-gray-700 mb-4'>The file contains duplicate tokens. Do you want to proceed with the submission?</p>
-            <div className='flex justify-end gap-3'>
-              <button
-                className='px-4 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700 transition'
-                onClick={handleAllowDuplicates}
-              >
-                Yes, Allow Duplicates
-              </button>
-              <button
-                className='px-4 py-2 bg-red-600 text-white rounded-lg text-sm font-medium hover:bg-red-700 transition'
-                onClick={handleDisallowDuplicates}
-              >
-                No, Cancel
-              </button>
+        {showDuplicatePopup && (
+          <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60'>
+            <div className='bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+              <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Duplicate Tokens Detected</h2>
+              <p className='text-gray-700 mb-4'>The file contains duplicate tokens. Do you want to proceed with the submission?</p>
+              <div className='flex justify-end gap-3'>
+                <button
+                  className='px-4 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700 transition'
+                  onClick={handleAllowDuplicates}
+                >
+                  Yes, Allow Duplicates
+                </button>
+                <button
+                  className='px-4 py-2 bg-red-600 text-white rounded-lg text-sm font-medium hover:bg-red-700 transition'
+                  onClick={handleDisallowDuplicates}
+                >
+                  No, Cancel
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
+
+        {showDisconnectPopup && (
+          <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60'>
+            <div className='bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+              <h2 className='text-2xl font-semibold text-gray-800 mb-4'>Disconnect Wallet</h2>
+              <p className='text-gray-700 mb-4'>Are you sure you want to disconnect your wallet?</p>
+              <div className='flex justify-end gap-3'>
+                <button
+                  className='px-4 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700 transition'
+                  onClick={handleConfirmDisconnect}
+                >
+                  Yes, Disconnect
+                </button>
+                <button
+                  className='px-4 py-2 bg-red-600 text-white rounded-lg text-sm font-medium hover:bg-red-700 transition'
+                  onClick={handleCancelDisconnect}
+                >
+                  No, Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+
+      </div>
     </div>
   );
 }
